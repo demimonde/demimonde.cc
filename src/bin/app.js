3c9b6de64003de6a1beecaafb1da0658698800dd
Anton <anton@adc.sh>
2018-12-10 03:18:38 +0300

    signin + static

--- src/bin/app.js
+++ src/bin/app.js
@@ -1,12 +1,75 @@
 import core from '@idio/core'
+import { facebookDialogUrl, exchange, graphGet } from '@demimonde/graph'
 
-(async () => {
+const { CLIENT_ID, SECRET, SESSION_KEY } = process.env
+
+;(async () => {
   const { app, router, url } = await core({
-
+    static: {
+      use: true,
+      root: 'static',
+    },
+    session: { use: true, keys: [SESSION_KEY || 'the-key'] },
   }, { port: process.env.PORT || 5000 })
   router.get('/', async (ctx, next) => {
-    ctx.body = 'hello demimonde'
+    const token = ctx.session.token
+    const l = token ? '<a href="/signout">Sing out</a>' : '<a href="/auth"><img src="fb.png"></a>'
+    const w = ctx.session.name ? `welcome back ${ctx.session.name}` : ''
+    ctx.body = `<!doctype html>
+<html>hello demimonde
+${w}
+<br>
+${l}</html>
+`
     await next()
+  })
+  router.get('/auth', async (ctx) => {
+    if (!CLIENT_ID) {
+      ctx.body = 'CLIENT_ID is not set on the app.'
+      ctx.status = 500
+      return
+    }
+    const state = Math.floor(Math.random() * 10000)
+    ctx.session.state = state
+    const redirect = `http://${ctx.host}/redirect`
+    const u = facebookDialogUrl({
+      client_id: CLIENT_ID,
+      redirect_uri: redirect,
+      scope: 'manage_pages',
+      state,
+    })
+    ctx.redirect(u)
+  })
+  router.get('/signout', async (ctx) => {
+    ctx.session.token = null
+    ctx.redirect('/')
+  })
+  router.get('/redirect', async (ctx) => {
+    if (!SECRET) {
+      ctx.body = 'SECRET is not set on the app.'
+      return
+    }
+    const redirect = `http://${ctx.host}/redirect`
+    const { state } = ctx.query
+    if (state != ctx.session.state) {
+      ctx.body = 'Wrong state'
+      ctx.status = 500
+      return
+    }
+    ctx.session.state = null
+    if (!ctx.query.code) throw new Error('Code Not Found.')
+
+    const token = await exchange({
+      client_id: CLIENT_ID,
+      client_secret: SECRET,
+      code: ctx.query.code,
+      redirect_uri: redirect,
+    })
+    ctx.session.token = token
+    const data = await graphGet('/me', token, {}, true)
+    ctx.session.name = data.name
+    ctx.session.id = data.id
+    ctx.redirect('/')
   })
   app.use(router.routes())
   console.log('Started on %s', url)
